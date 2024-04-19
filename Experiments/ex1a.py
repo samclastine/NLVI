17fda69cfa25866d3ccbdb84f87b5d23d99475bf
Sam Clastine <samclastine1225@gmail.com>
2024-04-19 10:45:13 +0100

    new Commit

--- Experiments/ex1a.py
+++ Experiments/ex1a.py
@@ -82,7 +82,7 @@ Vega-lite Json: """
                 eval_result = None
                 _error = None
                 try:
-                    pred_json = json.loads(pred)[0]
+                    pred_json = json.loads(pred)
                     pred_json['data'].clear()
                     pred_json['data']['url'] = 'https://raw.githubusercontent.com/nl4dv/nl4dv/master/examples/assets/data/' + dataFile
                     truth_json = ast.literal_eval(truth)
