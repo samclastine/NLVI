fcfd80d6894675750561bba0266482d45a25e31a
Sam Clastine <samclastine1225@gmail.com>
2024-05-14 21:58:20 +0100

    new

--- models/openai_models.py
+++ models/openai_models.py
@@ -1,6 +1,6 @@
 from langchain_community.chat_models import ChatOpenAI
 
 
-def initialize_openai_model(model_id):
-    model =  ChatOpenAI(model_name=model_id)
+def initialize_openai_model(model_id, temperature):
+    model =  ChatOpenAI(model_name=model_id, temperature=temperature)
     return model
